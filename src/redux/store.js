7f2d20daf0fc1016589723f30614e6644721df2f
MenendezON <menendezon@gmail.com>
2023-08-29 22:33:44 +0000

    Update store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import missionReducer from './missions/missionsSlice';
+import rocketsReducer from './rockets/rocketsSlice';
 import dragonsReducer from './dragons/dragonsSlice';
 
-const store = configureStore({
-  reducer: {
-    dragons: dragonsReducer,
+const rootReducer = combineReducers({
+  dragons: dragonsReducer,
+  mission: missionReducer,
+  rockets: rocketsReducer,
+});
 
-  },
+const store = configureStore({
+  reducer: rootReducer,
 });
 
 export default store;
