6550ec64297fa1eda3851745f759738e7bc612f4
RicardoEgas <rj_egas@hotmail.com>
2023-08-31 10:41:14 +0200

    Add rockets profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,7 +1,23 @@
-const MyProfile = () => (
-  <div>
-    <p>MyProfile</p>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const MyProfile = () => {
+  const { rockets } = useSelector((state) => state.rockets);
+  const reserveRocket = rockets.filter((rocket) => rocket.reserved);
+
+  return (
+    <>
+      <div>
+        <h2>My Rockets</h2>
+        <ul>
+          {
+            reserveRocket.map((rocket) => (
+              <li key={rocket.id}>{rocket.name}</li>
+            ))
+          }
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default MyProfile;
